6ea368c089f7a5b147aadba7edea60df5e439c8b
MercyEarls <mercyearls1@gmail.com>
2023-03-14 17:28:52 -0700

    Add Code

--- src/components/SelectVote1.js
+++ src/components/SelectVote1.js
@@ -1,7 +1,7 @@
 import '../styles/main.css'
 import Button from './Button';
 
-const Home = () => {
+const Home = () => { 
     return (
         <div className="text-center w-full h-screen flex items-center justify-center">
             <div className='w-32 h-32 rounded-full bg-green-500'></div>
